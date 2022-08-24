a6e1af154df3addd4a944a957be07dc74bda0e0b
Sravya <sravya.veeravalli123@gmail.com>
2022-08-25 01:45:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class AffiliationsList extends React.Component {
+  render() {
+    return(
+      <li>
+        {this.props.affil}
+      </li>
+    )
+  }
+}
+
+class StarWars extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      image: null,
+      name: null,
+      height: null,
+      homeworld: null,
+      affiliations: [],
+    }
+  }
+
+  getNewCharacter() {
+    const randNumber = Math.ceil(Math.random() * 88)
+    const url = `https://rawcdn.githack.com/akabab/starwars-api/0.2.1/api/id/${randNumber}.json`
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        this.setState({
+          image: data.image,
+          name: data.name,
+          hieght: data.height,
+          homeworld: data.homeworld,
+          affiliations: data.affiliations,
+        })
+        
+      })
+  }
+
+  render() {
+
+    const affiliation = this.state.affiliations.map((affil, i) => {
+      return <AffiliationsList key={i} affil={affil} />
+    })
+
+    return(
+      <div>
+        <img src={this.state.image} alt={`${this.state.name}'s image`} width={"400px"} height={"400px"}/>
+        <h1>{this.state.name}</h1>
+        <p>Height: {this.state.height}</p>
+        <p>HomeWorld: {this.state.homeworld}</p>
+        <p className="affili">Affiliations -</p>
+        <ul>
+          <li>
+            {affiliation}
+          </li>
+        </ul>
+        <button 
+          type="button" 
+          className="btn" 
+          onClick={() => this.getNewCharacter()}
+        >
+          Randomize character
+        </button>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <StarWars />
       </header>
     </div>
   );
